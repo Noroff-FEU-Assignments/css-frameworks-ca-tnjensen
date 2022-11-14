3bbc572793b5e300f93f8f1b66e3b1e1bef01ca4
tnjensen <thomasnorupjensen@gmail.com>
2022-11-14 15:35:02 +0100

    Configure tailwind add gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'heading-red': '#E92569',
+        'heading-bg': '#EFEFEF',
+      }
+    },
+  },
+  plugins: [],
+}
